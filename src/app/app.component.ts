f832051c5149a333f82f4aeb59a94ccbdc01b1fb
traumurlaub <istanbultrip2072@gmail.com>
2022-08-14 20:19:34 +0300

    Improve time.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,12 +21,12 @@ import {
 export class AppComponent implements OnInit {
   public now = new Date();
   public targetDate = new Date(2072, 7, 8); // 8 июля
-  public years = 0;
-  public months = 0;
-  public days = 0;
-  public hours = 0;
-  public minutes = 0;
-  public seconds = 0;
+  public years = `0`;
+  public months = `0`;
+  public days = `0`;
+  public hours = `0`;
+  public minutes = `0`;
+  public seconds = `0`;
 
   constructor() {
   }
@@ -39,16 +39,20 @@ export class AppComponent implements OnInit {
   }
 
   private _update() {
-    this.years = differenceInYears(this.targetDate, new Date())
-    let leftTime = subYears(this.targetDate, this.years);
-    this.months = differenceInMonths(leftTime, new Date());
-    leftTime = subMonths(leftTime, this.months);
-    this.days = differenceInDays(leftTime, new Date());
-    leftTime = subDays(leftTime, this.days);
-    this.hours = differenceInHours(leftTime, new Date());
-    leftTime = subHours(leftTime, this.hours);
-    this.minutes = differenceInMinutes(leftTime, new Date());
-    leftTime = subMinutes(leftTime, this.minutes);
-    this.seconds = differenceInSeconds(leftTime, new Date());
+    this.years = differenceInYears(this.targetDate, new Date()).toString()
+    let leftTime = subYears(this.targetDate, parseInt(this.years));
+    this.months = differenceInMonths(leftTime, new Date()).toString();
+    leftTime = subMonths(leftTime, parseInt(this.months));
+    this.days = differenceInDays(leftTime, new Date()).toString();
+    leftTime = subDays(leftTime, parseInt(this.days));
+    this.hours = differenceInHours(leftTime, new Date()).toString();
+    this.hours = this.hours.length < 2 ? `0${this.hours}` : this.hours;
+    leftTime = subHours(leftTime, parseInt(this.hours));
+    this.minutes = differenceInMinutes(leftTime, new Date()).toString();
+    this.minutes = this.minutes.length < 2 ? `0${this.minutes}` : this.minutes;
+    leftTime = subMinutes(leftTime, parseInt(this.minutes));
+    this.seconds = differenceInSeconds(leftTime, new Date()).toString();
+    this.seconds = this.seconds.length < 2 ? `0${this.seconds}` : this.seconds;
+
   }
 }
