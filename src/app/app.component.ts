0dedbb8b091d8c923800742830b54be56179f850
traumurlaub <istanbultrip2072@gmail.com>
2022-08-14 17:23:44 +0300

    Version without copy.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   public now = new Date();
-  public targetDate = new Date(2072, 6, 8); // 8 июля
-  public targetTime = this.targetDate.getTime();
+  public targetDate = new Date(2072, 7, 8); // 8 июля
+  public targetTime: number;
   public years = 0;
   public months = 0;
   public days = 0;
@@ -29,42 +29,42 @@ export class AppComponent implements OnInit {
   private _update() {
     const currentDate = new Date();
     const currentTime = currentDate.getTime();
-    let secondsLeft = (this.targetTime - currentTime) / 1000;
     this.years = this._calculateYear(currentDate, this.targetDate);
-    //
-    // this.months = secondsLeft / 2678400;
-    // secondsLeft = secondsLeft % 2678400;
-    //
-    // this.days = secondsLeft / 86400;
-    // secondsLeft = secondsLeft % 86400;
-    //
-    // this.hours = secondsLeft / 3600;
-    // secondsLeft = secondsLeft % 3600;
-    //
-    // this.minutes = secondsLeft / 60;
-    // this.seconds = secondsLeft % 60;
-    //
-    // this.years = this._pad(this.years, 2);
-    // this.months = this._pad(this.years, 2);
-    // this.days = this._pad(this.days, 2);
-    // this.hours = this._pad(this.hours, 2);
-    // this.minutes = this._pad(this.minutes, 2);
-    // this.seconds = this._pad(this.seconds, 2);
+    const newTargetDate = new Date(this.targetDate.getTime());
+    newTargetDate.setFullYear(new Date().getFullYear()) // установили ту же дату, но на текущий год.
+    if (newTargetDate < currentDate) {
+      newTargetDate.setFullYear((new Date().getFullYear() + 1));
+    }
+    this.targetTime = newTargetDate.getTime();
+    let secondsLeft = (this.targetTime - currentTime) / 1000;
+
+    this.months = Math.floor(secondsLeft / 2592000);
+    secondsLeft = secondsLeft % 2592000;
+
+    this.days = Math.floor(secondsLeft / 86400);
+    secondsLeft = secondsLeft % 86400;
+
+    this.hours = Math.floor(secondsLeft / 3600);
+    secondsLeft = secondsLeft % 3600;
+
+    this.minutes = Math.floor(secondsLeft / 60);
+    this.seconds =  Math.floor(secondsLeft % 60);
   }
 
   private _calculateYear(currentDate: Date, targetDate: Date): number {
+    const currentDateCopy = new Date(currentDate.getTime());
     const targetDateCopy = new Date(targetDate.getTime());
     targetDateCopy.setFullYear(new Date().getFullYear());
-    return currentDate > targetDateCopy ?
-      this.targetDate.getFullYear() - currentDate.getFullYear() - 1 :
-      this.targetDate.getFullYear() - currentDate.getFullYear();
+    return currentDateCopy > targetDateCopy ?
+      targetDate.getFullYear() - currentDate.getFullYear() - 1 :
+      targetDate.getFullYear() - currentDate.getFullYear();
   }
 
-  private _pad(num: number, size: number): number {
-    let s = Math.round(num) + "";
-    while (s.length < size) s = "0" + s;
-    return parseInt(s);
-  }
+  // private _pad(num: number, size: number): number {
+  //   let s = Math.round(num) + "";
+  //   while (s.length < size) s = "0" + s;
+  //   return parseInt(s);
+  // }
 
 
 }
