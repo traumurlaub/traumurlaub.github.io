e7517508e48a5dca6fa2108eb7253f0891bcb1af
traumurlaub <istanbultrip2072@gmail.com>
2022-08-14 18:35:34 +0300

    Use ngx-date-fns for calculate date difference like a crab.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import {Component, OnInit} from '@angular/core';
+import {
+  differenceInYears,
+  differenceInMonths,
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInSeconds,
+  subYears,
+  subMonths,
+  subDays,
+  subHours,
+  subMinutes,
+} from 'date-fns'
 
 @Component({
   selector: 'app-root',
@@ -7,8 +20,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
   public now = new Date();
-  public targetDate = new Date(2072, 7, 8); // 8 июля
-  public targetTime: number;
+  public targetDate = new Date(2072, 7, 15); // 8 июля
   public years = 0;
   public months = 0;
   public days = 0;
@@ -27,44 +39,16 @@ export class AppComponent implements OnInit {
   }
 
   private _update() {
-    const currentDate = new Date();
-    const currentTime = currentDate.getTime();
-    this.years = this._calculateYear(currentDate, this.targetDate);
-    const newTargetDate = new Date(this.targetDate.getTime());
-    newTargetDate.setFullYear(new Date().getFullYear()) // установили ту же дату, но на текущий год.
-    if (newTargetDate < currentDate) {
-      newTargetDate.setFullYear((new Date().getFullYear() + 1));
-    }
-    this.targetTime = newTargetDate.getTime();
-    let secondsLeft = (this.targetTime - currentTime) / 1000;
-
-    this.months = Math.floor(secondsLeft / 2592000);
-    secondsLeft = secondsLeft % 2592000;
-
-    this.days = Math.floor(secondsLeft / 86400);
-    secondsLeft = secondsLeft % 86400;
-
-    this.hours = Math.floor(secondsLeft / 3600);
-    secondsLeft = secondsLeft % 3600;
-
-    this.minutes = Math.floor(secondsLeft / 60);
-    this.seconds =  Math.floor(secondsLeft % 60);
+    this.years = differenceInYears(this.targetDate, new Date())
+    let leftTime = subYears(this.targetDate, this.years);
+    this.months = differenceInMonths(leftTime, new Date());
+    leftTime = subMonths(leftTime, this.months);
+    this.days = differenceInDays(leftTime, new Date());
+    leftTime = subDays(leftTime, this.days);
+    this.hours = differenceInHours(leftTime, new Date());
+    leftTime = subHours(leftTime, this.hours);
+    this.minutes = differenceInMinutes(leftTime, new Date());
+    leftTime = subMinutes(leftTime, this.minutes);
+    this.seconds = differenceInSeconds(leftTime, new Date());
   }
-
-  private _calculateYear(currentDate: Date, targetDate: Date): number {
-    const currentDateCopy = new Date(currentDate.getTime());
-    const targetDateCopy = new Date(targetDate.getTime());
-    targetDateCopy.setFullYear(new Date().getFullYear());
-    return currentDateCopy > targetDateCopy ?
-      targetDate.getFullYear() - currentDate.getFullYear() - 1 :
-      targetDate.getFullYear() - currentDate.getFullYear();
-  }
-
-  // private _pad(num: number, size: number): number {
-  //   let s = Math.round(num) + "";
-  //   while (s.length < size) s = "0" + s;
-  //   return parseInt(s);
-  // }
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {DateFnsModule} from "ngx-date-fns";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DateFnsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
